f58484d271a67be8b7bbadc9efbbda1d024acaae
Mark <mverleg.noreply@gmail.com>
2020-03-28 20:51:39 +0100

    Charset index types and tests

--- src/charset.rs
+++ src/charset.rs
@@ -17,6 +17,7 @@ impl Charset {
     /// Empty if the input contains duplicates.
     pub fn try_new<'a>(data: impl Into<&'a str>) -> Option<Self> {
         let data = data.into();
+        assert!(data.len() > 0);
         let mut seen = HashSet::new();
         let mut values = Vec::with_capacity(data.len());
         for character in data.chars() {
@@ -31,6 +32,7 @@ impl Charset {
         })
     }
 
+    /// Number of characters.
     pub fn len(&self) -> usize {
         self.values.len()
     }
@@ -41,5 +43,43 @@ impl Index<usize> for Charset {
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.values[index]
+    }
+}
+
+impl Index<u32> for Charset {
+    type Output = char;
+
+    fn index(&self, index: u32) -> &Self::Output {
+        &self[index as usize]
+    }
+}
+
+impl Index<u64> for Charset {
+    type Output = char;
+
+    fn index(&self, index: u64) -> &Self::Output {
+        &self[index as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_charset() {
+        let charset = Charset::try_new("abc");
+        assert!(charset.is_some());
+        let charset = charset.unwrap();
+        assert_eq!(charset.len(), 3);
+        assert_eq!(charset[0u32], 'a');
+        assert_eq!(charset[1u32], 'b');
+        assert_eq!(charset[2u32], 'c');
+    }
+
+    #[test]
+    fn invalid_charset() {
+        let charset = Charset::try_new("aba");
+        assert!(charset.is_none());
     }
 }
